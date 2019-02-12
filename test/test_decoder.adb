45e813daa7a4cf677663ee639d65b8ecd8c03e76
Romain Beguet <beguet@adacore.com>
2019-02-12 11:00:17 +0100

    Remove time delays in test decoder.

--- test/test_decoder.adb
+++ test/test_decoder.adb
@@ -5,20 +5,12 @@ with GB; use GB;
 with Decoder; use Decoder;
 
 procedure Test_Decoder is
-   Next    : Time;
-   Period  : constant Time_Span := Milliseconds (1);
-
    GB : GB_T;
 begin
    Init (GB);
    Load ("mem/rom", GB, 16#0000#);
 
-   Next := Clock + Period;
-
    loop
       Emulate_Cycle (GB);
-
-      delay until Next;
-      Next := Next + Period;
    end loop;
 end Test_Decoder;
