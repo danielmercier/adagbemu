cd7f8662cdb3937ba23a4fe1b8374e2b1deb1b62
Daniel Mercier <mercier@adacore.com>
2019-02-12 12:16:10 +0100

    Add test for push/pop instructions

--- test/test_instructions.adb
+++ test/test_instructions.adb
@@ -1,7 +1,30 @@
+with HAL; use HAL;
+with GB; use GB;
 with CPU; use CPU;
 with Instructions; use Instructions;
 
 procedure Test_Instructions is
+   GB : GB_T;
+
+   procedure Test_Push (GB : in out GB_T) is
+      Val : constant Uint16 := 16#4578#;
+   begin
+      Set_Reg (GB.CPU, HL, Val);
+      PUSH (GB.CPU, HL);
+      Set_Reg (GB.CPU, HL, Val + 10);
+      PUSH (GB.CPU, HL);
+      Set_Reg (GB.CPU, HL, Val + 20);
+      PUSH (GB.CPU, HL);
+
+      POP (GB.CPU, BC);
+      pragma Assert (Reg (GB.CPU, BC) = Val + 20);
+      POP (GB.CPU, BC);
+      pragma Assert (Reg (GB.CPU, BC) = Val + 10);
+      POP (GB.CPU, BC);
+      pragma Assert (Reg (GB.CPU, BC) = Val);
+   end Test_Push;
 begin
-   null;
+   Init (GB);
+
+   Test_Push (GB);
 end Test_Instructions;
